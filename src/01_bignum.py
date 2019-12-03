8dcfcfd3dec6a3ced7b361b7197392bd655172e8
VulpineReynard <milare8@gmail.com>
2019-12-03 15:25:57 -0500

    finished first two files

--- src/01_bignum.py
+++ src/01_bignum.py
@@ -2,3 +2,11 @@
 # (try doing the same thing in the JS console and see what it outputs)
 
 # YOUR CODE HERE
+
+pwr = 65536
+base = 2
+
+def print_result(base_num, pwr_num):
+  print(base_num ** pwr_num)
+
+print_result(base, pwr)
